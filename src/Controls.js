b9bf2fcba9dc2a2ed4b8376c8c80546f4f74cfc2
许雄龙 <xionglong.xu@gmail.com>
2019-04-06 11:32:57 +0800

    update: prevent low resolution texture

--- src/Controls.js
+++ src/Controls.js
@@ -1,7 +1,6 @@
 import Rete from 'rete';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dog from './dog.jpg'
 import * as THREE from 'three';
 
 export class TextInputControl extends Rete.Control {
@@ -31,7 +30,7 @@ export class ImageSelectControl extends Rete.Control {
 		let inputStyle = {
 			display: "none"
 		};
-		let imageStyle = {
+		let canvasStyle = {
 			width: "140px",
 			height: "140px"
 		};
@@ -43,25 +42,31 @@ export class ImageSelectControl extends Rete.Control {
 		let onChange = (e) => {
 			let reader = new FileReader();
 			let file = this.inputElement.files[0];
-			reader.readAsDataURL(file);
 			reader.onload = (e) => {
-				this.imageElement.src = e.target.result;
-			};
+				let image = document.createElement("img");
+				image.addEventListener("load", (e) => {
+					let texture = new THREE.Texture(image);
+					this.emitter.materialWriter.uniforms[this.key] = {value: texture};
+					texture.needsUpdate = true;
 
+					let context = this.canvasElement.getContext("2d");
+					context.drawImage(image, 0, 0);
+				}, false);
+				image.src = e.target.result;
+			};
+			reader.readAsDataURL(file);
 		};
 
-		let onImageRef = (el) => {
-			this.imageElement = el;
-			let texture = new THREE.Texture(this.imageElement);
-        	this.emitter.materialWriter.uniforms[this.key] = {value: texture};
-        	this.imageElement.onload = () => {
-            	texture.needsUpdate = true;
-        	};
+		let onCanvasRef = (el) => {
+			let context = el.getContext("2d");
+			context.fillStyle = "black";
+			context.fillRect(0, 0, el.width, el.height);
+			this.canvasElement = el;
 		}
 		this.template = (
 			<div style={divStyle}>
 				<input type="file" style={inputStyle} ref={(el) => {this.inputElement = el;}} onChange={onChange}/>
-				<img src={dog} alt="" style={imageStyle} onClick={onClick} ref={onImageRef}/>
+				<canvas style={canvasStyle} onClick={onClick} ref={onCanvasRef}/>
 			</div>
 		);
 	}
